284b7d861af9f006f160b184a0cad6a3e9bbbb31
Hyungjae Kim <hyungjae1130@gmail.com>
2021-01-16 18:28:23 +0400

    2021-01-16

---
+++ Silver/Jungol 590/cmake-build-debug/CMakeFiles/Jungol_590.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Jungol_590.dir/main.cpp.obj"
+  "Jungol_590.exe"
+  "Jungol_590.exe.manifest"
+  "Jungol_590.lib"
+  "Jungol_590.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Jungol_590.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
