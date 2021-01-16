a4ee8376fd1d5af0edb9c0a2cd6b4f69e01fc5c9
Hyungjae Kim <hyungjae1130@gmail.com>
2021-01-16 19:23:39 +0400

    2021-01-16

---
+++ Silver/Jungol 3528/cmake-build-debug/CMakeFiles/Jungol_3528.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Jungol_3528.dir/main.cpp.obj"
+  "Jungol_3528.exe"
+  "Jungol_3528.exe.manifest"
+  "Jungol_3528.lib"
+  "Jungol_3528.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Jungol_3528.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Silver/Jungol 3528/main.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n, cnt = 0;
+string word;
+vector<string> done;
+
+bool in(string a) {
+    for (int i = 0; i < done.size(); i++) {
+        if (a == done[i]) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+void process(string a, vector<pair<char, int>> possible) {
+    //cout << a;
+
+    if (a.length() == n) {
+        if (!in(a)) {
+            done.push_back(a);
+            cnt++;
+        }
+        return;
+    }
+
+    for (int i = 0; i < possible.size(); i++) {
+        if (a[a.length() - 1] != possible[i].first && possible[i].second > 0) {
+            string b = a;
+            b += possible[i].first;
+
+            vector<pair<char, int>> p = possible;
+            p[i].second--;
+
+            process(b, p);
+        }
+    }
+}
+
+int find(vector<pair<char, int>> p, char x) {
+    for (int i = 0; i < p.size(); i++) {
+        if (p[i].first == x) {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+int main() {
+    cin >> word;
+
+    n = word.length();
+
+    vector<pair<char, int>> p;
+    for (int j = 0; j < n; j++) {
+        int index = find(p, word[j]);
+
+        if (index != -1) {
+            p[index].second++;
+        }
+        else {
+            p.push_back(make_pair(word[j], 1));
+        }
+    }
+
+    for (int i = 0; i < p.size(); i++) {
+        string t = "";
+        t += p[i].first;
+
+        vector<pair<char, int>> s = p;
+        s[find(s, t[0])].second--;
+
+        process(t, s);
+    }
+
+    cout << cnt;
+
+    return 0;
+}
