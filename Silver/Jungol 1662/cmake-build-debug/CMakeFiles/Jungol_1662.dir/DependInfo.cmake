9c23c2feecf15de8733a71fc78b3a48e95bd0085
Hyungjae Kim <hyungjae1130@gmail.com>
2021-01-22 11:54:41 +0400

    2021-01-22

--- Silver/Jungol 1662/cmake-build-debug/CMakeFiles/Jungol_1662.dir/DependInfo.cmake
+++ Silver/Jungol 1662/cmake-build-debug/CMakeFiles/Jungol_1662.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "C:/Users/hyung/CLionProjects/Jungol 1662/main.cpp" "C:/Users/hyung/CLionProjects/Jungol 1662/cmake-build-debug/CMakeFiles/Jungol_1662.dir/main.cpp.obj"
+  "C:/Users/hyung/CLionProjects/USACO/Silver/Jungol 1662/main.cpp" "C:/Users/hyung/CLionProjects/USACO/Silver/Jungol 1662/cmake-build-debug/CMakeFiles/Jungol_1662.dir/main.cpp.obj"
   )
 set(CMAKE_CXX_COMPILER_ID "MSVC")
 
--- Silver/Jungol 1662/main.cpp
+++ Silver/Jungol 1662/main.cpp
@@ -2,75 +2,35 @@
 
 using namespace std;
 
-bool board[15][15];
-int n, maxC = 0;
+int n, ans;
+int board[15][15];
+bool check[30];
 
-void print() {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            cout << board[i][j] << " ";
-        }
-
-        cout << endl;
-    }
-
-    cout << endl << endl;
-}
-
-void process(int x, int y, bool b[15][15], int cnt) {
-    //print();
-
-    bool c[15][15];
-
-    for (int i = 0; i < 15; i++) {
-        for (int j = 0; j < 15; j++) {
-            c[i][j] = b[i][j];
-        }
-    }
-
-    int tx, ty;
-
-    if (x > y) {
-        tx = x - y;
-        ty = 0;
-    }
-    else {
-        ty = y - x;
-        tx = 0;
-    }
-
-    while (tx < n && ty < n && tx >= 0 && ty >= 0) {
-        c[tx][ty] = false;
-        tx++;
-        ty++;
-    }
-
-    tx = y + x;
-    ty = 0;
-
-    while (tx >= n || ty >= n) {
-        tx--;
-        ty++;
-    }
-
-    while (tx < n && ty < n && tx >= 0 && ty >= 0) {
-        c[tx][ty] = false;
-        tx--;
-        ty++;
-    }
-
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (c[i][j]) {
-                process(i, j, c, cnt + 1);
-            }
-        }
-    }
-
-    if (cnt > maxC) {
-        maxC = cnt;
+void bishop(int lvl, int cnt) {
+    if (lvl >= 2 * n) {
+        ans = max(ans, cnt);
         return;
+    }
+
+    int x = lvl, y = 0, flag = 0;
+
+    if (lvl >= n) {
+        x = n - 1;
+        y = lvl - n + 1;
+    }
+
+    for (x, y; x >= 0 && y < n; x--, y++) {
+        if (!check[n + x - y] && board[x][y] == 1) {
+            check[n + x - y] = true;
+            bishop(lvl + 1, cnt + 1);
+            check[n + x - y] = false;
+
+            flag = 1;
+        }
+    }
+
+    if (flag == 0) {
+        bishop(lvl + 1, cnt);
     }
 }
 
@@ -83,15 +43,9 @@ int main() {
         }
     }
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (board[i][j]) {
-                process(i, j, board, 1);
-            }
-        }
-    }
+    bishop(0, 0);
 
-    cout << maxC;
+    cout << ans;
 
     return 0;
 }
