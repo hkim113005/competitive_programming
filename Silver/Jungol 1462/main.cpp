16b9ea8cae2a7e57007d648a5a4c2579b26da54d
Hyungjae Kim <hyungjae1130@gmail.com>
2021-02-04 20:50:53 +0400

    2021-02-04

--- Silver/Jungol 1462/main.cpp
+++ Silver/Jungol 1462/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <queue>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -13,6 +14,10 @@ struct node {
         this->y = y;
         this->cnt = cnt;
     }
+
+    bool operator==(node const & a) const {
+        return a.x == this->x && a.y == this->y;
+    }
 };
 
 char map[55][55];
@@ -21,6 +26,9 @@ int N, M;
 int max_cnt = 0;
 node ans1 = node(0, 0, 0);
 node ans2 = node(0, 0, 0);
+int neighbors[4][3];
+int explored[3000][2];
+int explored_len = 0;
 
 void input() {
     cin >> N >> M;
@@ -32,38 +40,76 @@ void input() {
     }
 }
 
-vector<node> get_neighbors(node cur) {
-    vector<node> neighbors;
-
+int get_neighbors(node cur) {
     int x = cur.x;
     int y = cur.y;
     int cnt = cur.cnt;
 
+    int len = 0;
+
     if (x - 1 >= 0) {
         if (cur_map[x - 1][y] == 'L') {
-            neighbors.emplace_back(node(x - 1, y, cnt + 1));
+            neighbors[len][0] = x - 1;
+            neighbors[len][1] = y;
+            neighbors[len][2] = cnt + 1;
+
+            len++;
         }
     }
 
     if (x + 1 < N) {
         if (cur_map[x + 1][y] == 'L') {
-            neighbors.emplace_back(node(x + 1, y, cnt + 1));
+            neighbors[len][0] = x + 1;
+            neighbors[len][1] = y;
+            neighbors[len][2] = cnt + 1;
+
+            len++;
         }
     }
 
     if (y - 1 >= 0) {
         if (cur_map[x][y - 1] == 'L') {
-            neighbors.emplace_back(node(x, y - 1, cnt + 1));
+            neighbors[len][0] = x;
+            neighbors[len][1] = y - 1;
+            neighbors[len][2] = cnt + 1;
+
+            len++;
         }
     }
 
     if (y + 1 < M) {
         if (cur_map[x][y + 1] == 'L') {
-            neighbors.emplace_back(node(x, y + 1, cnt + 1));
+            neighbors[len][0] = x;
+            neighbors[len][1] = y + 1;
+            neighbors[len][2] = cnt + 1;
+
+            len++;
         }
     }
 
-    return neighbors;
+    return len;
+}
+
+bool in(int x, int y) {
+    for (int i = 0; i < explored_len; i++) {
+        if (explored[i][0] == x && explored[i][1] == y) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+void print() {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            cout << cur_map[i][j] << " ";
+        }
+
+        cout << endl;
+    }
+
+    cout << endl << endl;
 }
 
 void solve(int x, int y) {
@@ -75,23 +121,21 @@ void solve(int x, int y) {
         }
     }
 
-    node possible_ans_1 = node(x, y, 0);
-    node possible_ans_2 = node(0, 0, 0);
+    int possible_ans = 0;
 
     frontier.push(node(x, y, 0));
 
+    explored_len = 0;
+
+    explored[explored_len][0] = x;
+    explored[explored_len][1] = y;
+
+    explored_len++;
+
     while (true) {
         if (frontier.empty()) {
-            if (possible_ans_2.cnt > max_cnt) {
-                max_cnt = possible_ans_2.cnt;
-
-                ans1.x = possible_ans_1.x;
-                ans1.y = possible_ans_1.y;
-                ans1.cnt = possible_ans_1.cnt;
-
-                ans2.x = possible_ans_2.x;
-                ans2.y = possible_ans_2.y;
-                ans2.cnt = possible_ans_2.cnt;
+            if (possible_ans > max_cnt) {
+                max_cnt = possible_ans;
             }
 
             return;
@@ -103,16 +147,20 @@ void solve(int x, int y) {
 
         cur_map[front.x][front.y] = 'X';
 
-        if (front.cnt > possible_ans_2.cnt) {
-            possible_ans_2.x = front.x;
-            possible_ans_2.y = front.y;
-            possible_ans_2.cnt = front.cnt;
+        if (front.cnt > possible_ans) {
+            possible_ans = front.cnt;
         }
 
-        vector<node> neighbors = get_neighbors(front);
+        int len = get_neighbors(front);
 
-        for (int i = 0; i < (int)neighbors.size(); i++) {
-            frontier.push(neighbors[i]);
+        for (int i = 0; i < len; i++) {
+            if (!in(neighbors[i][0], neighbors[i][1])) {
+                frontier.push(node(neighbors[i][0], neighbors[i][1], neighbors[i][2]));
+                explored[explored_len][0] = neighbors[i][0];
+                explored[explored_len][1] = neighbors[i][1];
+
+                explored_len++;
+            }
         }
     }
 }
@@ -123,12 +171,40 @@ int main() {
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < M; j++) {
             if (map[i][j] == 'L') {
-                solve(i, j);
+                int k = 0;
+
+                if (i - 1 >= 0) {
+                    if (map[i - 1][j] == 'L') {
+                        k++;
+                    }
+                }
+
+                if (i + 1 < N) {
+                    if (map[i + 1][j] == 'L') {
+                        k++;
+                    }
+                }
+
+                if (j - 1 >= 0) {
+                    if (map[i][j - 1] == 'L') {
+                        k++;
+                    }
+                }
+
+                if (j + 1 < M) {
+                    if (map[i][j + 1] == 'L') {
+                        k++;
+                    }
+                }
+
+                if (k == 1 || k == 2) {
+                    solve(i, j);
+                }
             }
         }
     }
 
-    cout << ans2.cnt;
+    cout << max_cnt;
 
     return 0;
 }
