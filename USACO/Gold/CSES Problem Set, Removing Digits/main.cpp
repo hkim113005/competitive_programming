d4c661cef3077a74aee598b41a511fd5331d6e43
Hyungjae Kim <hyungjae1130@gmail.com>
2022-01-27 23:06:49 +0400

    2022-01-27

--- USACO/Gold/CSES Problem Set, Removing Digits/main.cpp
+++ USACO/Gold/CSES Problem Set, Removing Digits/main.cpp
@@ -1,6 +1,43 @@
 #include <iostream>
 
+#define INF (int)1e9
+
+using namespace std;
+
+int n;
+int dp[1000005];
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cin >> n;
+
+    for (int i = 0; i <= n; i++) {
+        dp[i] = INF;
+    }
+    dp[0] = 0;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = i; j > 0; j /= 10) {
+            dp[i] = min(dp[i - j % 10] + 1, dp[i]);
+        }
+    }
+
+    cout << dp[n] << endl;
+
     return 0;
 }
+
+/*
+ * 170
+ * 163
+ * 157
+ * 150 -> 151, 152, 153, 154, 155, 156, 157, 158, 159
+ * 151 -> 152, 156
+ * 152 -> 153, 157
+ * 153 -> 154, 158
+ * 154 -> 155, 160
+ * 155 -> 156, 161
+ * 156 -> 157, 162
+ * 157 -> 158, 163
+ * 158 -> 159, 164
+ * 159 -> 160, 165
+ */
