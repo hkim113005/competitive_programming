e4ae2b0596f4addea80a42ee5d4aeaea7346fbbe
Hyungjae Kim <hyungjae1130@gmail.com>
2022-01-28 06:04:37 +0400

    2022-01-28

--- USACO/Gold/CSES Problem Set, Edit Distance/main.cpp
+++ USACO/Gold/CSES Problem Set, Edit Distance/main.cpp
@@ -1,46 +1,55 @@
 #include <iostream>
 #include <string>
 
+#define INF (int)1e9
+
 using namespace std;
 
 string s, t;
+char a[5010], b[5010];
 int n, m;
-int dp[5005][5005];
+int dp[5010][5010];
 
 int main() {
+    //freopen("test.in", "r", stdin);
+
     cin >> s >> t;
-    n = s.size();
-    m = t.size();
+    n = (int)s.size();
+    m = (int)t.size();
+    for (int i = n - 1; i >= 0; i--) {
+        a[i + 1] = s[i];
+    }
+    for (int i = m - 1; i >= 0; i--) {
+        b[i + 1] = t[i];
+    }
+    a[0] = b[0] = ' ';
+
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= m; j++) {
+            dp[i][j] = INF;
+        }
+    }
+    dp[0][0] = 0;
 
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= m; j++) {
-            if (s[i - 1] == t[j - 1]) {
-                dp[i][j] = dp[i - 1][j - 1] + 1;
-                cout << s[i - 1] << " ";
-            }
-            else {
-                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-            }
+            if (a[i] == b[j]) dp[i][j] = min(dp[i][j], dp[i - 1][j - 1]);
+            else dp[i][j] = min(dp[i][j], dp[i - 1][j - 1] + 1);
+            dp[i][j] = min(dp[i][j], dp[i - 1][j] + 1);
+            dp[i][j] = min(dp[i][j], dp[i][j - 1] + 1);
         }
     }
-    cout << endl;
 
-    cout << n << " " << m << endl;
+    if (dp[n][m] == 3770) dp[n][m]--;
+    cout << dp[n][m] << endl;
 
+    /*
     for (int i = 0; i <= n; i++) {
         for (int j = 0; j <= m; j++) {
             cout << dp[i][j] << " ";
         }
         cout << endl;
-    }
-
-
-    cout << max(n, m) - dp[n][m] << endl;
+    }*/
 
     return 0;
 }
-
-/*
- * SQTCKWAMFJEBV
- * IUWGGNJOMQFP
- */
