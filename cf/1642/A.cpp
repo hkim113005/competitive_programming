75210afdefacc27b81d98448426d9cd91154ffb9
Hyungjae Kim <hyungjae1130@gmail.com>
2022-03-13 11:08:48 +0400

    vim trial and library

---
+++ cf/1642/A.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <queue>
+#include <math.h>
+#include <algorithm>
+
+using namespace std;
+
+int t, n;
+
+void solve() {
+    cin >> n;
+    cout << n << endl;
+}
+
+int main() {
+    cin >> t;
+    while (t--) solve();
+}
