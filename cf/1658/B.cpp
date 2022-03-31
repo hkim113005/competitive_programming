476f273753a7152fd1d38a2258861b640407cae6
Hyungjae Kim <hyungjae1130@gmail.com>
2022-03-31 12:35:48 +0400

    2022-03-30

---
+++ cf/1658/B.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using pi = pair<int, int>;
+
+template<class T> using p = pair<T, T>;
+template<class T> using v = vector<T>;
+template<class T> using pq = priority_queue<T>;
+template<class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+
+#define trav(a, x) for (auto& a : x)
+#define rdm(a, b) uniform_int_distribution<int>(a, b)(rng)
+
+#define len(x) (int)(x).size()
+#define mp make_pair
+#define pb emplace_back
+#define ft first
+#define sd second
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+#define ins insert
+
+template<class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, true : false; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, true : false; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+int const MOD = 998244353;
+int const MX = (int)1e5 + 5;
+int const N = (int)5e5 + 5;
+int const INF = (int)1e9;
+// ll const INF = (int)1e15;
+
+int in() {
+    int x;
+    cin >> x;
+    return x;
+}
+
+ll fact(int x) {
+    ll res = 1;
+    for (int i = 1; i <= x; i++) {
+        res *= i;
+        res %= MOD;
+    }
+    return res;
+}
+
+int t, n;
+
+void solve() {
+    n = in();
+    if (n % 2 == 1) {
+        cout << 0 << endl;
+        return;
+    }
+    cout << fact(n / 2) * fact(n / 2) % MOD << endl;
+}
+
+int32_t main() {
+    cin.tie(0)->sync_with_stdio(0);
+    cin.exceptions(cin.failbit);
+    cin >> t;
+    while (t--) solve();
+    return 0;
+}
+
---
+++ cf/1658/D1.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using pi = pair<int, int>;
+
+template<class T> using p = pair<T, T>;
+template<class T> using v = vector<T>;
+template<class T> using pq = priority_queue<T>;
+template<class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+
+#define trav(a, x) for (auto& a : x)
+#define rdm(a, b) uniform_int_distribution<int>(a, b)(rng)
+
+#define len(x) (int)(x).size()
+#define mp make_pair
+#define pb emplace_back
+#define ft first
+#define sd second
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+#define ins insert
+
+template<class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, true : false; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, true : false; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+int const MOD = (int)1e9 + 7;
+int const MX = (int)1e5 + 5;
+int const N = (int)5e5 + 5;
+int const INF = (int)1e9;
+// ll const INF = (int)1e15;
+
+int in() {
+    int x;
+    cin >> x;
+    return x;
+}
+
+int t, n;
+
+void solve() {
+    int l = in(), r = in();
+    int a[N];
+    n = r - l + 1;
+    for (int i = 0; i < n; i++) {
+        a[i] = in();
+    }
+    
+    int b[20];
+    for (int i = 0; i < 18; i++) {
+        b[i] = 0;
+    }
+    for (int i = 0; i < n; i++) {
+        int x = 0;
+        while (a[i] > 0) {
+            b[x] += (a[i] & 1);
+            x++;
+            a[i] /= 2;
+        }
+    }
+
+    int ans[20];
+    for (int i = 0; i < 18; i++) {
+        if (b[i] > n / 2) {
+            ans[i] = 1;
+        }
+        else {
+            ans[i] = 0;
+        }
+    }
+
+    int x = 0;
+    for (int i = 0; i < 18; i++) {
+        x += ans[i] * (int)pow(2, i);
+    }
+    cout << x << endl;
+}
+
+int32_t main() {
+    cin.tie(0)->sync_with_stdio(0);
+    cin.exceptions(cin.failbit);
+    cin >> t;
+    while (t--) solve();
+    return 0;
+}
+
